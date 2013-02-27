e83c5eed14a1c6a2907d966dd6fb4b57319ae9f7
andonic <andonic@a73d61cc-dd5f-463d-9e47-042b297c4eee>
2013-02-27 10:29:49 +0000

    [ADD]: Load Library error trace

--- trunk/control/VideoManFactory.cpp
+++ trunk/control/VideoManFactory.cpp
@@ -467,7 +467,22 @@ VideoInput *VideoManFactory::createVideoInputWindows( const VMInputIdentificatio
 		hinstLib = LoadLibrary( moduleName.c_str() );
 	if ( hinstLib == NULL )
 	{
+		DWORD err = GetLastError();
+		LPVOID lpMsgBuf;
+		FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER | 
+              FORMAT_MESSAGE_FROM_SYSTEM | 
+              FORMAT_MESSAGE_IGNORE_INSERTS,
+              NULL, 
+              GetLastError(), 
+              MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), // Default language
+              (LPTSTR) &lpMsgBuf, 
+              0, 
+              NULL);
+
 		cerr << "ERROR: unable to load DLL "  << moduleName << endl;
+		printf("ERROR: %s\n",(LPCTSTR)lpMsgBuf);
+		// Free the buffer.
+		LocalFree( lpMsgBuf );
 		return NULL;
 	}
 
